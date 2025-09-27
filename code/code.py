991f2ef8c1fb4a969b6e76fc1fe9fffcb89384b9
Vango Romano <tobie@lolness>
2025-09-27 17:14:29 +0200

    Functionnal code with correct output... Need report

--- code/code.py
+++ code/code.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from skimage.metrics import structural_similarity as ssim
 
 def read_image(name: str, path: str = '../images') -> np.ndarray:
     img = cv2.imread(path + '/' + name, cv2.IMREAD_GRAYSCALE)
@@ -27,9 +28,8 @@ def normalize(img: np.ndarray, target: int = 255) -> np.ndarray:
 
 def add_noise(img: np.ndarray, mean: float = 0.0, std: float = 1.0) -> np.ndarray:
     noise = np.random.normal(loc = mean, scale = std, size = img.shape)
-    print_range(noise)
-    img_noised = img + noise
-    return np.clip(img_noised, a_min = 0, a_max = 255)
+    img_noised = img.astype(np.float32) + noise
+    return np.clip(img_noised, a_min = 0, a_max = 255).astype(np.uint8)
 
 def filterFT(img: np.ndarray, h: np.ndarray) -> np.ndarray:
     F_img = np.fft.fft2(img)
@@ -47,12 +47,30 @@ def jpeg_compress(img: np.ndarray, quality: int = 90) -> np.ndarray:
     compressed_img = cv2.imdecode(encoded_img, cv2.IMREAD_UNCHANGED)
     return compressed_img
 
+def PSNR(img_1: np.ndarray, img_2: np.ndarray, max_value=255) -> np.ndarray:
+    mse = MSE(img_1, img_2)
+    if mse == 0:
+        return 100
+    return 20 * np.log10(max_value/np.sqrt(mse)) # Multiple definitions possibles ??
+
+def print_quality(img_1: np.ndarray, img_2: np.ndarray):
+    print(f"MSE: {MSE(img_1, img_2)}")
+    print(f"PSNR: {PSNR(img_1, img_2)}")
+    print(f"SSIM: {ssim(img_1, img_2)}")
+    print("LPIPS")
+
 def image_processing(img: np.ndarray):
-    print_range(img)
-    noise_1 = add_noise(img)
-    noise_2 = add_noise(img)
+    noise_1 = add_noise(img, std = 0.01)
+    noise_2 = add_noise(img, std = 0.5)
     jpeg_1 = jpeg_compress(img)
     jpeg_2 = jpeg_compress(img, quality = 50)
+    images = [noise_1, noise_2, jpeg_1, jpeg_2]
+    titles = ["Gaussian noise with sigma = 0.01", "Gaussian noise with sigma = 0.5", "Jpeg compression 90", "Jpeg compression 50"]
+    for i in range(len(images)):
+        print("\n==============================================")
+        print(f"{titles[i]}")
+        print_range(images[i])
+        print_quality(img, images[i])
     
 
 
