9a4782e742a11bd7e8e032601f540f112536e64b
Vango Romano <tobie@lolness>
2025-10-22 13:58:14 +0200

    Create images

--- code/tp4.py
+++ code/tp4.py
@@ -4,6 +4,8 @@ import argparse
 from skimage import io, color, util
 from scipy.fftpack import dctn, idctn
 import skimage
+import cv2
+from skimage.transform import rescale
 
 
 def read_image(name: str) -> np.ndarray:
@@ -22,12 +24,13 @@ def normalize(img: np.ndarray, target: float = 1.0) -> np.ndarray:
 
 def crop_img(img: np.ndarray, tlx: int, tly: int, brx: int, bry: int) -> np.ndarray:
     assert len(img.shape) == 2, "Image must be 2 dimensions !"
-    return img[tlx:brx, tly:bry]
+    return img[tly:bry, tlx:brx]
 
 
 def insert_img(img: np.ndarray, img2: np.ndarray, tlx: int, tly: int) -> np.ndarray:
     result = img.copy()
-    result[tlx : tlx + img2.shape[0], tly : tly + img2.shape[1]] = img2
+    result[tly : tly + img2.shape[0], tlx : tlx + img2.shape[1]] = img2
+    result[result == 0] = img[result == 0]
     return result
 
 
@@ -47,6 +50,23 @@ def print_image(img: np.ndarray, title: str = "Image"):
     plt.show()
 
 
+def add_noise(img: np.ndarray, mean: float = 0.0, std: float = 1.0) -> np.ndarray:
+    noise = np.random.normal(loc=mean, scale=std, size=img.shape)
+    img_noised = img.astype(np.float32) + noise
+    return np.clip(img_noised, a_min=0, a_max=255).astype(np.uint8)
+
+
+def jpeg_compress(img: np.ndarray, quality: int = 90) -> np.ndarray:
+    params = [cv2.IMWRITE_JPEG_QUALITY, quality]
+    success, encoded_img = cv2.imencode(".jpg", img, params)
+
+    if not success:
+        print("Error when encoding")
+
+    compressed_img = cv2.imdecode(encoded_img, cv2.IMREAD_UNCHANGED)
+    return compressed_img
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Double JPEG compression")
     parser.add_argument(
@@ -55,7 +75,53 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     img = read_image(args.image)
-    cropped_img = crop_img(img, 250, 220, 370, 340)
+    cropped_img = crop_img(img, 197, 894, 345, 1000)
+    rescale_1_5 = skimage.transform.rescale(cropped_img, scale=1.5)
+    rescale_0_5 = skimage.transform.rescale(cropped_img, scale=0.5, anti_aliasing=True)
+    rotate_90 = skimage.transform.rotate(
+        cropped_img, angle=90, resize=False, preserve_range=True
+    )
+    rotate_45 = skimage.transform.rotate(
+        cropped_img, angle=45, resize=False, preserve_range=True
+    )
+    noise_0_5 = add_noise(cropped_img, std=0.5)
+    noise_1 = add_noise(cropped_img, std=1)
+    compression_90_50 = jpeg_compress(jpeg_compress(cropped_img, 90), 50)
+    compression_50_90 = jpeg_compress(jpeg_compress(cropped_img, 50), 90)
     print_image(img)
     print_image(cropped_img)
-    print_image(insert_img(img, cropped_img, 250, 400))
+    print_image(
+        insert_img(img, cropped_img, 578, 894), title="moved part without modification"
+    )
+    print_image(
+        insert_img(img, rescale_1_5, (1000 - 578) // 2, (1000 - 894) // 2),
+        title="moved part upscaled 2 times",
+    )
+    print_image(
+        insert_img(img, rescale_0_5, 480, 930),
+        title="moved part downscaled 2 times",
+    )
+    print_image(
+        insert_img(img, rotate_90, 578, 894),
+        title="moved part rotated 90",
+    )
+    print_image(
+        insert_img(img, rotate_45, 400, 894),
+        title="moved part rotated 45",
+    )
+    print_image(
+        insert_img(img, noise_0_5, 578, 894),
+        title="moved part noise 0.5",
+    )
+    print_image(
+        insert_img(img, noise_1, 578, 894),
+        title="moved part noise 1",
+    )
+    print_image(
+        insert_img(img, compression_50_90, 578, 894),
+        title="moved part compression Q50 then Q90",
+    )
+    print_image(
+        insert_img(img, compression_90_50, 578, 894),
+        title="moved part compression Q90 then Q50",
+    )
