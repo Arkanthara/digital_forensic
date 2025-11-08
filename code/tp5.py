9c20a30ad399516f4418ff901d5ac70afebf15c9
Vango Romano <vango@romano>
2025-11-08 15:24:32 +0100

    Part 1 and 2 done

--- code/tp5.py
+++ code/tp5.py
@@ -27,6 +27,39 @@ def lsb_extraction(img: np.ndarray, level: int = 0) -> np.ndarray:
     # return ((result & mask) > 0).astype(int)
     return result & mask
 
+def hide_message(img: np.ndarray, msg: str = "Hellloooowww !!!", key: int = 21, level: int = 0) -> np.ndarray:
+    # Create random number generator with given key
+    rng = np.random.default_rng(seed=key)
+    rows, cols = img.shape
+    layer = lsb_extraction(img, level)
+    result = img.copy() - layer
+    layer = layer.ravel()
+    msg_bits = 2**level * np.array(list(''.join(format(b, '08b') for b in msg.encode('utf-8')))).astype(int)
+    positions = rng.choice(np.arange(layer.shape[0]), size=msg_bits.shape[0])
+    layer[positions] = msg_bits
+    result += layer.reshape(rows, cols)
+    return result
+
+def get_message(img: np.ndarray, key: int = 21, length: int = 100, level: int = 0) -> str:
+    rng = np.random.default_rng(seed=key)
+    layer = lsb_extraction(img, level).ravel()
+    positions = rng.choice(np.arange(layer.shape[0]), size=length*8)
+    msg_bits = layer[positions]
+    msg_bits = (msg_bits > 0).astype(int)
+    msg_bits = ''.join(str(i) for i in msg_bits)
+    # We ignore here the decoding errors to retrieve a message even if we don't know the size of the message
+    return int(msg_bits, 2).to_bytes(len(msg_bits) // 8, 'big').decode('utf-8', errors='ignore')
+
+def bit_plane_visualization(img: np.ndarray, title: str = "Bit-planes of the image"):
+    plt.figure()
+    plt.suptitle(title)
+    for i in range(8):
+        plt.subplot(2, 4, i + 1)
+        plt.imshow(lsb_extraction(img, i), cmap='gray')
+        plt.axis('off')
+        plt.title(f'Level {i}')
+    plt.show()
+
 
 def crop_img(img: np.ndarray, tlx: int, tly: int, brx: int, bry: int) -> np.ndarray:
     assert len(img.shape) == 2, "Image must be 2 dimensions !"
@@ -50,13 +83,25 @@ def print_image(img: np.ndarray, title: str = "Image"):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Double JPEG compression")
-    # parser.add_argument(
-    #     "-i", "--image", required=True, help="Image to make double compression"
-    # )
+    parser = argparse.ArgumentParser(description="Steganography")
+    parser.add_argument(
+        "-i", "--image", help="Image"
+    )
+    parser.add_argument(
+        "-b", "--bit_plane", action="store_true", help="Display each bit-plane of the image"
+    )
+    parser.add_argument("-m", "--message", help="Message to hide !")
+    parser.add_argument("-p", )
     args = parser.parse_args()
 
-    a = np.ones((5, 5))
-    print(lsb_extraction(a))
-    print(lsb_extraction(a, 1))
-    print(lsb_extraction(a * 2, 1))
+    if args.bit_plane:
+        assert args.image, "Image must be given to show each bit-plane of the image !"
+        img = read_image(args.image)
+        bit_plane_visualization(img)
+    if args.message:
+        assert args.image, "Image must be given to show each bit-plane of the image !"
+        img = read_image(args.image)
+        img_msg = hide_message(img, msg=args.message, level=7)
+        print_image(img, title='Original image')
+        print_image(img_msg, title='Image with hidden message')
+        print(f"Recovered message: {get_message(img_msg)}")
