4eeda70b5f6c6c658ab9afc618873a0f8660f03b
Vango Romano <vango@romano>
2025-11-19 09:30:01 +0100

    backup

---
+++ code/mini_project.py
@@ -0,0 +1,25 @@
+import argparse
+import matplotlib.pyplot as plt
+import skimage as ski
+import numpy as np
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Mini project")
+    parser.add_argument("-f1", "--frame_1", help="Frame 1 of a video")
+    parser.add_argument("-f2", "--frame_2", help="Frame 2 of a video")
+    args = parser.parse_args()
+    img1 = ski.io.imread(args.frame_1, as_gray=True)
+    img2 = ski.io.imread(args.frame_2, as_gray=True)
+
+    plt.figure()
+    plt.suptitle("Changes between 2 consecutives frame in a video")
+    plt.subplot(1, 2, 1)
+    plt.axis("off")
+    plt.imshow(img1, cmap='gray')
+    plt.title("Frame 1")
+    plt.subplot(1, 2, 2)
+    plt.axis('off')
+    plt.imshow(ski.util.compare_images(img1, img2, method='diff'), cmap='gray')
+    plt.title("Change with two consecutives frames")
+    plt.show()
